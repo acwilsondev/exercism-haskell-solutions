2c5f2c5c44aac69f4f796cf1f3f9ec82ca5005e5
Aaron Wilson <acwilsoncs@gmail.com>
2026-03-04 08:30:25 -0700

    complete rna

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,4 +1,12 @@
 module DNA (toRNA) where
 
+-- Traverse a string, converting DNA to RNA
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = traverse dnaToRna
+
+dnaToRna :: Char -> Either Char Char
+dnaToRna 'G' = Right 'C'
+dnaToRna 'C' = Right 'G'
+dnaToRna 'T' = Right 'A'
+dnaToRna 'A' = Right 'U'
+dnaToRna x   = Left x
